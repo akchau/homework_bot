2e85144a05655f4fe73522586673aee7f6982005
akcaau <gllazrv@gmail.com>
2022-03-26 15:28:26 +0300

    Rewiev02

---
+++ exceptions/exceptions.py
@@ -0,0 +1,97 @@
+class NoTypeStr(Exception):
+    """Исключение, статус домашки тип данных не str.
+
+    Args:
+        Exceptions (class): Базовый класс исключений
+    """
+    def __init__(self, message='Некорректный тип данных. Ожидался str со статусом.'):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class NullStatus(Exception):
+    """Исключение, когда в статус незадокументирован.
+
+    Args:
+        Exceptions (class): Базовый класс исключений
+    """
+
+    def __init__(self, message='Пустой статус домашки.'):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class NoTypeDict(Exception):
+    """Исключение, когда в ответе API код не 200.
+
+    Args:
+        Exceptions (class): Базовый класс исключений
+    """
+    def __init__(self, message='Некорректный тип данных. Ожидался dict с домашкой.'):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class CodeApiError(Exception):
+    """Исключение, когда в ответе API код не 200.
+
+    Args:
+        Exceptions (class): Базовый класс исключений
+    """
+    def __init__(self, message='Код ответа НЕ 200.'):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class NoKeyHomeworks(Exception):
+    """Исключение, когда в ответе API нет ключа homeworks.
+
+    Args:
+        Exceptions (class): Базовый класс исключений
+    """
+    def __init__(self, message='В ответе нет ключа homeworks.'):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class NoTypeList(Exception):
+    """Исключение, в ответе API тип данных не list.
+
+    Args:
+        Exceptions (class): Базовый класс исключений
+    """
+    def __init__(self, message='Некорректный тип данных. Ожидался list с домашками.'):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
+
+
+class EmptyList(Exception):
+    """Исключение, в ответе API нет данных о домашке.
+
+    Args:
+        Exceptions (class): Базовый класс исключений
+    """
+    def __init__(self, message='За указанное время не было никаких домашек. Увеличьте время.'):
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        return self.message
--- homework.py
+++ homework.py
@@ -1,6 +1,4 @@
-
-"""
-Телеграм бот который отправит сообщение,
+"""Телеграм бот который отправит сообщения.
 если изменился статус проверки домашней работы в Яндекс.Практикум."
 """
 
@@ -11,22 +9,42 @@ from telegram import Bot
 import requests
 import time
 import logging
+from exceptions import exceptions
+from logging.handlers import RotatingFileHandler
 
 load_dotenv()
 
 logging.basicConfig(
     format='%(asctime)s - %(levelname)s - %(message)s',
+    encoding='utf-8',
     level=logging.DEBUG,
     filename='main.log',
-    filemode='w'
-    )
+    filemode='w',
+)
 
+formatter = logging.Formatter(
+    '%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+handler = RotatingFileHandler(
+    'my_logger.log',
+    maxBytes=50000000,
+    backupCount=5
+)
+logger.addHandler(handler)
+handler.setFormatter(formatter)
 
 PRACTICUM_TOKEN = os.getenv('PRACTICUM_TOKEN')
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 
-RETRY_TIME = 0.2
+TOKENS = [TELEGRAM_TOKEN, TELEGRAM_CHAT_ID, PRACTICUM_TOKEN]
+TELEGRAM_TOKENS = [TELEGRAM_TOKEN, TELEGRAM_CHAT_ID]
+
+
+RETRY_TIME = 20
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
 TIME_OF_PRACTICUM_OPENING = 1549962000
@@ -37,86 +55,27 @@ HOMEWORK_STATUSES = {
     'rejected': 'Работа проверена: у ревьюера есть замечания.'
 }
 
-MESSAGES = {
-    'no_tokens': 'Ошибка токенов окружения. Завершение работы',
-    'fail_api_request': 'Недоступен эндпоинт, ошибка АPI',
+INFO_MESSAGES = {
     'sent_message': 'Успешно отправлено сообщение в телеграмм',
-    'many_failure': 'Превышено допустимое количество ошибок',
     'stop_bot': 'Бот остановлен!',
-    'fail_api_response': 'API вернул некорректный ответ',
-    'no_key_homeworks': 'В ответе нет ключа homeworks',
-    'no_type_list': 'Некорректный тип данных ожидался list',
-    'empty_list': 'За указанное время не было никаких домашек',
     'no_new_status': 'Статус работы не поменялся',
-    'no_sent_message': 'Ошибка отправки сообщения',
-    'no_documenting_status': 'Не задукоментрирован статус',
-    'api_request_200': 'Код ответа - ОШИБКА'
 }
 
 
-class CodeApiError(Exception):
-    """Исключение, когда в ответе API код не 200.
-
-    Args:
-        Exceptions (class): Базовый класс исключений
-    """
-    pass
-
-
-class NoKeyHomeworks(Exception):
-    """Исключение, когда в ответе API нет ключа homeworks.
-
-    Args:
-        Exceptions (class): Базовый класс исключений
-    """
-    pass
+UNIVERSE_EXCEPT_MESSAGES = {
+    'no_sent_message': 'Ошибка отправки сообщения',
+    'fail_api_response': 'API вернул некорректный ответ',
+    'no_tokens': 'Ошибка токенов окружения. Завершение работы',
+    'fail_api_request': 'Недоступен эндпоинт, ошибка запроса к АPI',
+    'many_failure': 'Превышено допустимое количество ошибок / ошибка токенов.',
+}
 
 
-class NoTypeList(Exception):
-    """Исключение, в ответе API тип данных не list.
-
-    Args:
-        Exceptions (class): Базовый класс исключений
-    """
-    pass
-
-
-class EmptyList(Exception):
-    """Исключение, в ответе API нет данных о домашке.
-
-    Args:
-        Exceptions (class): Базовый класс исключений
-    """
-    pass
-
-
-def send_message(bot, message):
-    """Функция отправляет сообщение от телеграм бота.
-
-    Args:
-        bot (Bot): Телеграмм клиент бота, авторизованный по токену
-        message (str): Сообщение которое отправит бот
-    """
-    try:
-        bot.send_message(TELEGRAM_CHAT_ID, message)
-    except Exception:
-        message = MESSAGES.get('no_sent_message')
-        logging.error(message)
-        print(message)
-    else:
-        message = MESSAGES.get('sent_message')
-        logging.info(message)
-        print(message)
-    pass
-
-
-def stop_bot():
-    """Функция остановки бота
-    """
-    message_stop_bot = MESSAGES.get('stop_bot')
-    logging.info(message_stop_bot)
-    print(message_stop_bot)
-    sys.exit(0)
+def check_tokens():
+    """Проверка значения переменных TOKENS."""
+    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
+        return True
+    return False
 
 
 def get_api_answer(current_timestamp) -> dict:
@@ -130,46 +89,17 @@ def get_api_answer(current_timestamp) -> dict:
     """
     timestamp = current_timestamp or int(time.time())
     # timestamp = timestamp - TIME_OF_PRACTICUM_OPENING
-    # timestamp = str(timestamp)
     params = {'from_date': timestamp}
     headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
     response = requests.get(ENDPOINT, headers=headers, params=params)
     if response.status_code != 200:
-        raise CodeApiError
+        raise exceptions.CodeApiError
     homeworks = response.json()
     return homeworks
 
 
-def check_tokens() -> bool:
-    """
-    Проверяет наличие токенов в виртуальном окружении.
-    Возвращает True, если все токены на месте"
-
-    Returns:
-        bool: True, если все токены в окружении на месте.
-    """
-    if PRACTICUM_TOKEN and TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
-        return True
-    else:
-        return False
-
-
-def check_telegram_tokens() -> bool:
-    """
-    Проверяет наличие только телеграмм токенов в виртуальном окружении.
-    Возвращает True, если все токены на месте"
-
-    Returns:
-        bool: True, если все токены в окружении на месте.
-    """
-    if TELEGRAM_TOKEN and TELEGRAM_CHAT_ID:
-        return True
-    else:
-        return False
-
-
 def check_response(response: dict) -> dict:
-    """Функция проверяет ответ API на:
+    """Функция проверяет ответ API.
     - Наличие ключа 'homeworks',
     - Тип данных dict,
     - Тип данных по ключу homeworks: list,
@@ -186,13 +116,37 @@ def check_response(response: dict) -> dict:
         dict: Параметры посленей домашней работы
     """
     if 'homeworks' in response:
-        if type(response.get('homeworks')) is not list:
-            raise NoTypeList
-        if len(response.get('homeworks')) == 0:
-            raise EmptyList
+        homeworks = response.get('homeworks')
+        homework = homeworks[0]
+        if type(homeworks) is not list:
+            raise exceptions.NoTypeList
+        if len(homeworks) == 0:
+            raise exceptions.EmptyList
+        if type(homework) is not dict:
+            raise exceptions.NoTypeDict
+        if 'status' not in homework:
+            raise exceptions.NullStatus
+        else:
+            status = homework.get('status')
+        if type(status) is not str:
+            raise exceptions.NoTypeStr
+        if status not in HOMEWORK_STATUSES:
+            raise exceptions.NullStatus
     else:
-        raise NoKeyHomeworks
+        raise exceptions.NoKeyHomeworks
     return response.get('homeworks')[0]
+
+
+def check_status(homework: dict) -> str:
+    """Функция получает на вход данные о ДЗ и возвращает статус.
+
+    Args:
+        homework (dict): Словарь с параметрами домашней работы.
+
+    Returns:
+        str: Статус последней домашней работы
+    """
+    return homework.get('status')
 
 
 def parse_status(homework: dict) -> str:
@@ -210,103 +164,70 @@ def parse_status(homework: dict) -> str:
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
 
-def check_status(homework: dict) -> str:
-    """Функция получает на вход данные о ДЗ и возвращает статус"
+def send_message(bot, message):
+    """Функция отправляет сообщение от телеграм бота.
 
     Args:
-        homework (dict): Словарь с параметрами домашней работы.
-
-    Returns:
-        str: Статус последней домашней работы
+        bot (Bot): Телеграмм клиент бота, авторизованный по токену
+        message (str): Сообщение которое отправит бот
     """
-    return homework.get('status')
-
-
-def api_incorrect(bot, message):
-    logging.error(message)
-    print(message)
-    send_message(bot, message)
+    try:
+        bot.send_message(TELEGRAM_CHAT_ID, message)
+    except Exception:
+        message = UNIVERSE_EXCEPT_MESSAGES.get('no_sent_message')
+        logging.error(message)
+    else:
+        message = INFO_MESSAGES.get('sent_message')
+        logging.info(message)
     pass
 
 
+def stop_bot():
+    """Функция остановки бота."""
+    message_stop_bot = INFO_MESSAGES.get('stop_bot')
+    logging.info(message_stop_bot)
+    sys.exit(0)
+
+
 def main():
-    """Главная функция!
-    """
-    current_timestamp = int(time.time())
-    last_status = ''
+    """Главная функция."""
+    current_timestamp = int(time.time() - TIME_OF_PRACTICUM_OPENING)
+    logging.info('Бот запущен')
+    last_status: str = ''
     count_failure = 0
-    message = MESSAGES.get('no_tokens')
-    if check_telegram_tokens():
+    message = UNIVERSE_EXCEPT_MESSAGES.get('no_tokens')
+    if TELEGRAM_TOKENS:
         bot = Bot(token=TELEGRAM_TOKEN)
-        while check_tokens():
-            if count_failure < 10:
-                try:
-                    response = get_api_answer(current_timestamp)
-                except CodeApiError:
-                    message = MESSAGES.get('api_request_200')
-                    count_failure += 1
-                    api_incorrect(bot, message)
-                except Exception:
-                    message = MESSAGES.get('fail_api_request')
-                    count_failure += 1
-                    api_incorrect(bot, message)
-                else:
-                    except_messages = {
-                        NoKeyHomeworks: 'no_key_homeworks',
-                        NoTypeList: 'no_type_list',
-                        EmptyList: 'empty_list',
-                        Exception: 'fail_api_response',
-                    }
-                    try:
-                        homework = check_response(response)
-                    except NoKeyHomeworks:
-                        message = MESSAGES.get(except_messages[NoKeyHomeworks])
-                        count_failure += 1
-                        api_incorrect(bot, message)
-                    except NoTypeList:
-                        message = MESSAGES.get(except_messages[NoTypeList])
-                        count_failure += 1
-                        api_incorrect(bot, message)
-                    except EmptyList:
-                        message = MESSAGES.get(except_messages[EmptyList])
-                        count_failure += 1
-                        api_incorrect(bot, message)
-                    except Exception:
-                        message = MESSAGES.get(except_messages[Exception])
-                        count_failure += 1
-                        api_incorrect(bot, message, count_failure)
-                    else:
-                        current_stautus = check_status(homework)
-                        if current_stautus == '':
-                            message = MESSAGES.get('no_documenting_status')
-                            count_failure += 1
-                            logging.error(message)
-                            print(message)
-                            send_message(bot, message)
-                        else:
-                            if current_stautus != last_status:
-                                message = parse_status(homework)
-                                last_status = homework.get('status')
-                                logging.info(message)
-                                print(message)
-                                send_message(bot, message)
-                                time.sleep(RETRY_TIME)
-                            else:
-                                message = MESSAGES.get('no_new_status')
-                                logging.debug(message)
-                                print(message)
-                finally:
-                    time.sleep(RETRY_TIME)
-            else:
-                message = MESSAGES.get('many_failure')
-                logging.critical(message)
-                print(message)
+        while check_tokens() and count_failure < 3:
+            try:
+                response = get_api_answer(current_timestamp)
+            except Exception as error:
+                error_message = UNIVERSE_EXCEPT_MESSAGES.get('fail_api_request')
+                logging.error(f'Ошибка: {error_message} - {error}')
+                count_failure += 1
+                continue
+            try:
+                homework = check_response(response)
+            except Exception as error:
+                error_message = UNIVERSE_EXCEPT_MESSAGES.get('fail_api_response')
+                logging.error(f'{error_message} - {error}')
+                count_failure += 1
+                continue
+            current_stautus = check_status(homework)
+            if current_stautus != last_status:
+                message = parse_status(homework)
+                last_status = current_stautus
+                logging.info(message)
                 send_message(bot, message)
-                stop_bot()
-    else:
-        logging.critical(message)
-        print(message)
-        stop_bot()
+            else:
+                message = INFO_MESSAGES.get('no_new_status')
+                logging.debug(message)
+            time.sleep(RETRY_TIME)
+        else:
+            message = UNIVERSE_EXCEPT_MESSAGES.get('many_failure')
+            send_message(bot, message)
+    logging.critical(message)
+    stop_bot()
 
 
 if __name__ == '__main__':
